3ecad3e3536a2ae86f3c3712a3f8dff5f19aa5ce
sunxiangyu <1340518678@qq.com>
2022-09-29 15:31:50 +0800

    修改

--- run.py
+++ run.py
@@ -2,23 +2,23 @@ import pytest
 import schedule
 
 
-def job1():
-    pytest.main(["./cases/trade/test_reconnect.py", "-v", "-s"])
+# def job1():
+#     pytest.main(["./cases/trade/test_reconnect.py", "-v", "-s"])
 
 
 def job2():
-    pytest.main(["./cases/trade/test_insert_sql.py", "-v", "-s", "--reruns", "3"])
+    pytest.main(["./cases/trade/test_insert_sql.py", "-v", "-s"])
 
 
 def job3():
-    pytest.main(["./cases/trade/test_fast_market.py"])
+    pytest.main(["./cases/trade/test_fast_market.py", "-v", "-s"])
 
 
 def job4():
-    pytest.main(["./cases/trade/test_fast_market1.py"])
+    pytest.main(["./cases/trade/test_fast_market_1.py", "-v", "-s"])
 
 
-schedule.every(1).day.do(job1)  # 每隔 1天运行一次 job 函数
+# schedule.every(1).day.do(job1)  # 每隔 1天运行一次 job 函数
 schedule.every(10).minutes.do(job2)  # 每隔 10 分钟运行一次 job 函数
 schedule.every(10).minutes.do(job3)  # 每隔 10 分钟运行一次 job 函数
 schedule.every(10).minutes.do(job4)  # 每隔 10 分钟运行一次 job 函数
